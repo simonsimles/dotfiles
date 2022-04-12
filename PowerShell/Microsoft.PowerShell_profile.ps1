87e209fe2965457a9f3c924410821590397ce84f
simonsimles <simonsimles@simles.de>
2022-04-12 09:44:11 +0200

    New CDF module

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -10,9 +10,29 @@ Import-Module posh-git
 Set-Alias -Name fcd -Value Invoke-FuzzySetLocation
 
 Import-Module ~\Documents\github\cdf.ps\Cdf.psm1
-Set-Alias -Name cdf -Value Set-FuzzyDirectory
+if (Get-Module Cdf) {
+    Remove-Alias cd
+    Set-Alias -Name cd -Value Set-FuzzyDirectory
+}
 
 Import-Module PSReadLine
 Set-PSReadLineOption -EditMode Vi
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+function OnViModeChange {
+    if ($args[0] -eq 'Command') {
+        # Set the cursor to a blinking block.
+        Write-Host -NoNewLine "`e[1 q"
+    } else {
+        # Set the cursor to a blinking line.
+        Write-Host -NoNewLine "`e[5 q"
+    }
+}
+Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:OnViModeChange
+
+Set-PSReadLineOption -BellStyle None
+
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
--- nvim/init.lua
+++ nvim/init.lua
@@ -38,7 +38,8 @@ require'nvim-treesitter.configs'.setup {
 
 
 require'lspconfig'.powershell_es.setup{
-  bundle_path = 'c:/tools/PowerShellEditorServices',
+    bundle_path = 'c:/tools/PowerShellEditorServices',
+    filetypes = { "ps1", "psm1", "psd1" },
 }
 
 require'lspconfig'.kotlin_language_server.setup{}
